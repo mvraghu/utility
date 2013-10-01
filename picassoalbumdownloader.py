65cbac766436af517febc7e423acf8e05a70c4d3
mvraghu <mvraghu@gmail.com>
2013-10-01 10:35:54 +0530

    Update picassoalbumdownloader.py

--- picassoalbumdownloader.py
+++ picassoalbumdownloader.py
@@ -1,26 +1,5 @@
 #! /usr/bin/python
 
-# import os
-
-
-# def  sayhello():
-    
-#   os.chdir("./content")
-
-#   for dirname, dirnames, filenames in os.walk('.'):
-#     # print path to all subdirectories first.
-#       for subdirname in dirnames:
-#           print os.path.join(dirname, subdirname)
-
-#     # print path to all filenames.
-#     for filename in filenames:
-#         print os.path.join(dirname, filename)
-
-
-# sayhello
-
-
-
 import gdata.photos.service
 import gdata.media
 import gdata.geo
